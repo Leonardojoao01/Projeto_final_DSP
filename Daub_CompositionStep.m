ec9d51a11f59d6fe28ea6de41d83065a7b5107a6
leonardo joao <leonardo joao>
2016-12-22 20:02:09 -0200

    Verificar posicao do vetor

--- Daub_CompositionStep.m
+++ Daub_CompositionStep.m
@@ -9,25 +9,44 @@ function [ result ] = Daub_CompositionStep(v,n)
     denom = 4.0 * sqrt(2.0);
     result = zeros(n,1);
     
-    a = v(half + 1); 
-    b = v(n); 
+    a = v(half - 1); 
+    b = v(n - 1); 
     c = v(1); %????
     d = v(half);
     
     t1 = c - a; 
     t2 = b - d; 
     
-    %result(0) = uint8((c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom);
-    %result(1) = unit8((a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom);
+    %result(1) = uint8((c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom);
+    %result(2) = unit8((a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom);
     
     juca1 = (c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom;
     juca2 = (a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom;
     
-    result(1) = uint8(juca1);
-    result(2) = uint8(juca2);
+    result(1) = juca1;
+    result(2) = juca2;
     
     %
     % loop aqui
     %
+    j=3;
+    for i=1:half-1
+        offset1 = v(i);
+        offset2 = v(half+i);
+        
+        a = offset1;
+        b = offset2;
+        c = offset1+1;
+        d = offset2+1;
+        
+        t1 = c - a;
+        t2 = b - d;
+        
+      
+        result(j) = (c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom;
+        j = j+1;
+        result(j) = (a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom;
+        j = j+1;
+    end;
 end
 
--- script_teste.m
+++ script_teste.m
@@ -2,14 +2,16 @@
 %s = im2double('mario.jpg');
 %s = double(s);
 %s = s(1:1:4);
-n = 4;
+n = 16;
 s = zeros(n,1);
 
 for i=1:size(s,1)
-    juca = double(s);
-    juca(1) = 9.0;
-    juca(2) = 7.0;
-    juca(3) = 15.0;
-    juca(4) = 2.0;
+    %juca = double(s);
+    %juca(1) = 9.0;
+    %juca(2) = 7.0;
+    %juca(3) = 15.0;
+    %juca(4) = 2.0;
+    
+    juca = [7  6  9  3  1  15  10  12  9  13  10  11  2  11  3  6];
     result = Daub_CompositionStep(juca, n);
 end;
