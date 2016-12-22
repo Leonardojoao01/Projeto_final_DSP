851734b950234101de6b5119628ee11a6c4f04d6
leonardo joao <leonardo joao>
2016-12-22 20:59:30 -0200

    ...

--- Daub_CompositionStep.m
+++ Daub_CompositionStep.m
@@ -9,10 +9,10 @@ function [ result ] = Daub_CompositionStep(v,n)
     denom = 4.0 * sqrt(2.0);
     result = zeros(n,1);
     
-    a = v(half - 1); 
-    b = v(n - 1); 
-    c = v(1); %????
-    d = v(half);
+    a = double(v(half-1)); 
+    b = double(v(n-1)); 
+    c = double(v(1)); %????
+    d = double(v(half));
     
     t1 = c - a; 
     t2 = b - d; 
@@ -23,8 +23,8 @@ function [ result ] = Daub_CompositionStep(v,n)
     juca1 = (c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom;
     juca2 = (a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom;
     
-    result(1) = juca1;
-    result(2) = juca2;
+    result(1) = uint8(juca1);
+    result(2) = uint8(juca2);
     
     %
     % loop aqui
@@ -34,19 +34,22 @@ function [ result ] = Daub_CompositionStep(v,n)
         offset1 = v(i);
         offset2 = v(half+i);
         
-        a = offset1;
-        b = offset2;
-        c = offset1+1;
-        d = offset2+1;
+        a = double(offset1);
+        b = double(offset2);
+        c = double(offset1+1);
+        d = double(offset2+1);
+        %offset1 = offset1 +1;
+        %offset2 = offset2 +1;
         
         t1 = c - a;
         t2 = b - d;
         
       
-        result(j) = (c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom;
+        result(j) = uint8((c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom);
         j = j+1;
-        result(j) = (a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom;
+        result(j) = uint8((a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom);
         j = j+1;
+
     end;
 end
 
