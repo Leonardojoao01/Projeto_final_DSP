db1ee296875131d2c48ddedcd029eea34559a814
Huberto Kaiser Filho <betinhoh@gmail.com>
2016-12-21 23:50:23 -0200

    finished non standard decomposition

--- Daub_NonStandardDecomposition.m
+++ Daub_NonStandardDecomposition.m
@@ -1,4 +1,4 @@
-function [ output_args ] = Daub_NonStandardDecomposition( mat, rows, cols )
+function [ mat ] = Daub_NonStandardDecomposition( mat, rows, cols )
 
     h = rows;
     w = cols;
@@ -21,25 +21,25 @@ function [ output_args ] = Daub_NonStandardDecomposition( mat, rows, cols )
         end
 
 		if h >= 4
-			for (uint i = 0; i < w; i++)
+            for i=1:1:w
 			
-				for (uint j = 0; j < h; j++)
-					temp_col[j] = mat[j][i];
+                for j=1:1:h
+					temp_col(j) = mat(j,i);
                 end
                 
-				Daub_DecompositionStep(temp_col, h, normal, optimalFilters);
-
-				for (uint j = 0; j < h; j++)
-					mat[j][i] = temp_col[j];
+				vet = Daub_DecompositionStep(temp_col, h);
+                
+                for j=1:1:h
+					mat(j,i) = vet(j);
                 end
             end
         end
         
         if (w >= 4) 
-            w /= 2; 
+            w = w / 2; 
         end
         if (h >= 4) 
-            h /= 2; 
+            h = h / 2; 
         end
     end
 
