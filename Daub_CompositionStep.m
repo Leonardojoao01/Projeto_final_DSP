aa8ef4344f62161ed210b41a8f3bb868161cbfca
leonardo joao <leonardo joao>
2017-02-10 00:44:54 -0200

    Com problemas no composition

--- Daub_CompositionStep.m
+++ Daub_CompositionStep.m
@@ -8,25 +8,25 @@ function [ v ] = Daub_CompositionStep(v,n)
     denom = 4.0 * sqrt(2.0);
     result = zeros(1,length(n));
     
-    a = v(half-1); 
-    b = v(n-1); 
-    c = v(1); %????
-    d = v(half);
+    a = v(half); 
+    b = v(n); 
+    c = v(1);
+    d = v(half+1);
     
     t1 = c - a; 
     t2 = b - d; 
     
-    %result(1) = uint8((c + d + v3*(a + b) + sqrt3*(t1 + t2)) / denom);
-    %result(2) = unit8((a - b + v3*(c - d) + sqrt3*(t1 - t2)) / denom);
+    %result(1) = uint8((c + d + (3*(a + b)) + (sqrt(3)*(t1 + t2))) / denom)
+    %result(2) = uint8((a - b + (3*(c - d)) + (sqrt(3)*(t1 - t2))) / denom)
     
-    result(1) = (c + d + (3*(a + b)) + (sqrt(3)*(t1 + t2))) / denom;
-    result(2) = (a - b + (3*(c - d)) + (sqrt(3)*(t1 - t2))) / denom;
+    result(1) = (c + d + (3*(a + b)) + (sqrt(3)*(t1 + t2))) / denom
+    result(2) = (a - b + (3*(c - d)) + (sqrt(3)*(t1 - t2))) / denom
     
     %
     % loop aqui
     %
     j=3;
-    for i=1:1:half-1
+    for i=1:1:half
         offset1 = v(i);
         offset2 = v(half+i);
         
@@ -34,8 +34,6 @@ function [ v ] = Daub_CompositionStep(v,n)
         b = double(offset2);
         c = double(offset1+1);
         d = double(offset2+1);
-        %offset1 = offset1 +1;
-        %offset2 = offset2 +1;
         
         t1 = c - a;
         t2 = b - d;
